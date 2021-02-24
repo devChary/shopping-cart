a8afd9acd262016e19bac8f3bce5ebf0f95427d3
jagan <peddiniti.jagannadh@algoworks.com>
2021-02-25 01:29:14 +0530

    all products page mobile view done

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -17,18 +17,21 @@ const Header = () => {
 
     return (
         <header className="header">
-            <Link className="logo-container" to="/">
-                <img src="/static/assets/logo.png" alt="Sabka Bazaar Logo" className="logo" />
-            </Link>
+            <div className="items-wrapper">
+                <Link className="logo-container" to="/">
+                    <img src="/static/assets/logo.png" alt="Sabka Bazaar Logo" className="logo" />
+                </Link>
 
-            <nav className="user-nav">
-                <div className="user-nav__nav-item">
-                    <a href="#" className="user-nav__home">Home</a>
-                </div>
-                <div className="user-nav__nav-item">
-                    <a href="#" className="user-nav__products">Products</a>
-                </div>
-            </nav>
+                <nav className="user-nav">
+                    <Link className="user-nav__nav-item" to="/">
+                        Home
+                    </Link>
+                    <Link className="user-nav__nav-item" to="/products">
+                        Products
+                    </Link>
+
+                </nav>
+            </div>
 
             <div className="user-cart">
                 <div className="user-cart__logo-box">
