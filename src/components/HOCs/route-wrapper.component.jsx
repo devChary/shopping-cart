503644972fa73530e546be142ab9b6002d84f207
devChary <jagannadhachary7182@gmail.com>
2022-03-12 02:44:51 +0530

    adding rr6 in route HOC

--- src/components/HOCs/route-wrapper.component.jsx
+++ src/components/HOCs/route-wrapper.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import {Sidenav} from 'components/UI/organisms';
 
@@ -7,23 +7,27 @@ import './route-wrapper.styles.scss';
 
 export const WithSidebarRoute = ({ component: Component, ...rest }) => {
     return (
-        <Route {...rest} component={(props) => (
-            <div className="content" style={{ display: 'flex' }}>
-                <Sidenav /> {/* Sidebar ALWAYS VISIBLE */}
-                <Component {...props} />
-            </div>
-        )}
-        />
+        <Routes>
+            <Route {...rest} component={(props) => (
+                <div className="content" style={{ display: 'flex' }}>
+                    <Sidenav /> {/* Sidebar ALWAYS VISIBLE */}
+                    <Component {...props} />
+                </div>
+            )}
+            />
+         </Routes>
     )
 }
 
 export const WithoutSidebarRoute = ({ component: Component, ...rest }) => {
     return (
-        <Route {...rest} component={(props) => (
-            <div className="content">
-                <Component {...props} />
-            </div>
-        )}
-        />
+        <Routes>
+            <Route {...rest} component={(props) => (
+                <div className="content">
+                    <Component {...props} />
+                </div>
+            )}
+            />
+        </Routes>
     )
 }
